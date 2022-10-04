8c630d803307a982177551c261eed495e4474862
Jefferj <jefferj080@gmail.com>
2022-10-04 13:45:48 -0500

    Challenge Available

---
+++ ChallengeEight.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+/*
+ Enunciado: Available en API: Manejo de versiones
+ */
+
+if #available(iOS 12, macOS 10.12, *){
+    //Ejecutar las acciones a los iOS 12, macOS 10.12
+}else{
+    //Mantener el código con versiones anteriores
+}
